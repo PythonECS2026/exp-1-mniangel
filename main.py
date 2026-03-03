ed829b7cefd5d2d7abd5a53c95986a7669753197
mniangel <mnicheonsa@gmail.com>
2026-03-03 20:42:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # AIM: Create a personalized greeting message
 #      User enters name: eg: Shibu. and the program should print "Hello, Shibu" 
-# Coder: 
+# Coder: Angel Mishra 
 # Date:
 name=input("smiley")
 age=input("0")
